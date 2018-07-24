7b9c4faf623d50742e679fd7fb99845070e15a7b
Pi <praneshulleri@gmail.com>
2018-07-24 06:12:34 +0530

    .

---
+++ code_Kyoya_and_Photobooks.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string a;
+    int i;
+    int c=0;
+    cin>>a;
+   for(i=0;a[i]!='\0';i++)
+   {}//cout <<i;
+    c=26+25*(i);
+cout<<c;
+		return 0;
+}
